6599395035fc49ff966e1d5e631166aaa82e4cce
behzad <behzadfareed@gmail.com>
2020-11-20 15:40:56 +0330

    sss

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -21,13 +21,13 @@ class Api::ApiController < ApplicationController
         if params[:token]
             acc = Account.find_by(acc_id: params[:token])
             if acc.nil?
-                render json: { result: false, data: nil }, status: 401
+                render json: false, status: 401
                 return
             end
-            render json: {result: true, data: ActiveModel::SerializableResource.new(acc) }
+            render json:acc
             return
         else
-            render json: { result: false, data: nil }, status: 401
+            render json: false, status: 401
             return
         end
     end
